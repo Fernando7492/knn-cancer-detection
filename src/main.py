a2714c60b789af50a5c5ebfdaaa6cff8f33768a7
Fernando7492 <emidio8000@gmail.com>
2025-04-22 20:53:01 -0300

    feat: treino de modelo

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from src.outliers_detection import outliers_por_coluna
 from src.preprocessing import binarizar_col, escalar_col
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 data = pd.read_csv("data/breast-cancer-winsconsin-data.csv",sep=",",encoding="utf-8")
 data = data.drop(columns=['id'])
@@ -14,5 +16,9 @@ print(outliers_por_coluna(data))
 
 df_escalado = escalar_col(data)
 
+x = df_escalado.drop(columns='diagnosis')
+y = df_escalado['diagnosis']
 
-
+x_treino, x_teste, y_treino, y_teste = train_test_split(x,y,test_size=0.2,random_state=42)
+modelo_knn = KNeighborsClassifier(n_neighbors=5)
+modelo_knn.fit(x_treino,y_treino)
