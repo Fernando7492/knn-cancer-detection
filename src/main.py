b1eaf12e9b5bc5b54fbde274d328da10c081cce0
Fernando7492 <emidio8000@gmail.com>
2025-04-23 18:11:03 -0300

    feat: comparação entre MeuKnn e Sklearn_KNN

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from src.preprocessing import binarizar_col, escalar_col
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from src.testar_modelo import testar_modelo
+from src.MeuKnn import MeuKnn
 
 
 data = pd.read_csv("data/breast-cancer-winsconsin-data.csv",sep=",",encoding="utf-8")
@@ -20,12 +21,19 @@ df_escalado = escalar_col(data)
 
 x = df_escalado.drop(columns='diagnosis')
 y = df_escalado['diagnosis']
-
 x_treino, x_teste, y_treino, y_teste = train_test_split(x,y,test_size=0.2,random_state=42)
+
+#Sklearn - KNN
 modelo_knn = KNeighborsClassifier(n_neighbors=8)
 modelo_knn.fit(x_treino,y_treino)
-
 y_pred = modelo_knn.predict(x_teste)
 
+#MeuKnn
+meuKnn = MeuKnn(k=8)
+meuKnn.fit(x_treino.values,y_treino.values)
+y_pred_manual = meuKnn.predict(x_teste.values)
+
+
 testar_modelo(y_teste,y_pred)
+testar_modelo(y_teste,y_pred_manual)
 
