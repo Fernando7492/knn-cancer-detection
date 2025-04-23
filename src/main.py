a9b0d7dd1c0a27794818d2f9085e073542792f5e
Fernando7492 <emidio8000@gmail.com>
2025-04-22 21:03:06 -0300

    feat: Analise de acuracia do algortimo

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from src.outliers_detection import outliers_por_coluna
 from src.preprocessing import binarizar_col, escalar_col
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 data = pd.read_csv("data/breast-cancer-winsconsin-data.csv",sep=",",encoding="utf-8")
 data = data.drop(columns=['id'])
@@ -11,8 +12,8 @@ data = data.drop(columns=['id'])
 data = binarizar_col("diagnosis",data)
 
 #Verificando quantos outliers existem em cada coluna
-print("Outliers por coluna:")
-print(outliers_por_coluna(data))
+#print("Outliers por coluna:")
+#print(outliers_por_coluna(data))
 
 df_escalado = escalar_col(data)
 
@@ -22,3 +23,8 @@ y = df_escalado['diagnosis']
 x_treino, x_teste, y_treino, y_teste = train_test_split(x,y,test_size=0.2,random_state=42)
 modelo_knn = KNeighborsClassifier(n_neighbors=5)
 modelo_knn.fit(x_treino,y_treino)
+
+y_pred = modelo_knn.predict(x_teste)
+acuracia = accuracy_score(y_teste,y_pred)
+
+print(f'ACURACIA: {acuracia}')
