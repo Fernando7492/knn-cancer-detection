ab87ab726e7a471c1378f0d40c126026fac9c42f
Fernando7492 <emidio8000@gmail.com>
2025-04-23 16:34:34 -0300

    Feat: MeuKnn, init e fit criados

---
+++ src/MeuKnn.py
@@ -0,0 +1,23 @@
+import math
+import numpy as np
+
+class MeuKnn:
+    """Classe para calcular o K-vizinhos mais próximos, usando distância euclidiana.
+    """
+    def __init__(self, k=5):
+        """
+
+        Args:
+            k (int, optional): Número de vizinhos próximos que será usado pelo algortimo. Valor padrão = 5.
+        """
+        self.k = k
+        
+    def fit(self,x_treino, y_treino):
+        """Função para armazenar os valores de treino na classe
+
+        Args:
+            x_treino (Numpy array): Um array numpy contendo os atributos dos elementos
+            y_treino (Numpy array): Um array numpy contendo as classes dos elementos
+        """
+        self.x_treino = x_treino
+        self.y_treino = y_treino
