97b6d352d3169e4f8959210023fe32894cfb86e3
Fernando7492 <emidio8000@gmail.com>
2025-04-22 21:38:07 -0300

    feat: matriz de confusão

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 data = pd.read_csv("data/breast-cancer-winsconsin-data.csv",sep=",",encoding="utf-8")
 data = data.drop(columns=['id'])
@@ -33,6 +34,16 @@ acuracia = accuracy_score(y_teste,y_pred)
 #Report de classificação
 report = classification_report(y_teste,y_pred,target_names=['Benigno','Maligno'])
 
+#matriz de confusão
+cm = confusion_matrix(y_teste,y_pred)
+
 
 print(f'ACURACIA: {acuracia}')
 print(report)
+print("Matriz de Confusão:")
+print("                  Predito")
+print("              Benigno  Maligno")
+print(f"Real Benigno     {cm[0][0]:<8} {cm[0][1]}")
+print(f"Real Maligno     {cm[1][0]:<8} {cm[1][1]}")
+
+
