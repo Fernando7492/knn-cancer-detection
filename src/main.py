21fe49f3613232ae3f43a3330e948833e9740c5a
Fernando7492 <emidio8000@gmail.com>
2025-04-23 21:21:20 -0300

    fix: função removida apagada do import

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from src.outliers_detection import outliers_por_coluna
 from src.preprocessing import binarizar_col, escalar_col
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -13,10 +12,6 @@ data = data.drop(columns=['id'])
 
 #Transformando a coluna diagnosis em binário
 data = binarizar_col("diagnosis",data)
-
-#Verificando quantos outliers existem em cada coluna
-#print("Outliers por coluna:")
-#print(outliers_por_coluna(data))
 
 df_escalado = escalar_col(data)
 
